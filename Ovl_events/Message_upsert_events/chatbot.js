1a5ddbe5f945a984858a1119f80803c5c7dd2f66
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-07-04 20:18:16 +0000

    Add files via upload

---
+++ Ovl_events/Message_upsert_events/chatbot.js
@@ -0,0 +1,59 @@
+const axios = require('axios');
+const { ChatbotConf } = require('../../DataBase/chatbot');
+
+async function chatbot(ms_org, verif_Groupe, texte, repondre) {
+  try {
+    if (!texte) return;
+
+    const config = await ChatbotConf.findByPk('1');
+    if (!config) return;
+
+    let enabledIds = [];
+    try {
+      enabledIds = JSON.parse(config.enabled_ids || '[]');
+    } catch {
+      enabledIds = [];
+    }
+
+    const localActif = enabledIds.includes(ms_org);
+    const globalActif = verif_Groupe ? config.chatbot_gc === 'oui' : config.chatbot_pm === 'oui';
+
+    if (localActif || globalActif) {
+      const response = await axios.post(
+        'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAk9Mtmnk8SuuCf7T9z8Hkw5dPxiAMVc8U',
+        {
+          contents: [
+            {
+              parts: [{ text: texte }],
+            },
+          ],
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      const data = response.data;
+
+      if (data.candidates && data.candidates.length > 0) {
+        let reponseTexte = data.candidates[0]?.content?.parts?.[0]?.text || "";
+
+        reponseTexte = reponseTexte
+          .replace(/Google/gi, 'AINZ')
+          .replace(/un grand modèle linguistique/gi, 'OVL-MD-V2-CHAT-BOT');
+
+        if (reponseTexte) {
+          repondre(reponseTexte);
+        }
+      } else {
+        repondre("Aucune réponse adaptée de l'API.");
+      }
+    }
+  } catch (err) {
+    console.error("Erreur dans chatbot :", err);
+  }
+}
+
+module.exports = chatbot;
---
+++ Ovl_events/Message_upsert_events/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  rankAndLevelUp: require('./rank&levelup'),
+  lecture_status: require('./lecture_status'),
+  like_status: require('./like_status'),
+  presence: require('./presence'),
+  dl_status: require('./dl_status'),
+  antidelete: require('./antidelete'),
+  antitag: require('./antitag'),
+  antilink: require('./antilink'),
+  antibot: require('./antibot'),
+  getLid: require('./cache'),
+  mention: require('./mention'),
+  eval_exec: require('./eval_exec'),
+  antimention: require('./antimention'),
+  chatbot: require('./chatbot')
+};

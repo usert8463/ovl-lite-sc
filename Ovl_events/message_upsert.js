6ab2f13fcc52d23f258f19445bbe2acbbca9ea95
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-07-24 11:50:32 +0000

    Update message_upsert.js

--- Ovl_events/message_upsert.js
+++ Ovl_events/message_upsert.js
@@ -44,6 +44,7 @@ async function message_upsert(m, ovl) {
     if (m.type !== 'notify') return;
     const ms = m.messages?.[0];
     if (!ms?.message) return;
+    console.log(ms?.message);
     addMessage(ms.key.id, ms);
 
     const mtype = getContentType(ms.message);

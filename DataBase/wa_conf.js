9e6e9511aca8d8f48f5800d65473db6fe5eeae0b
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-08-03 08:27:01 +0000

    Update wa_conf.js

--- DataBase/wa_conf.js
+++ DataBase/wa_conf.js
@@ -4,70 +4,77 @@ const db = config.DATABASE;
 
 let sequelize;
 
-if (!db) { 
-  sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.db',
-    logging: false,
-  });
+if (!db) {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.db',
+    logging: false,
+  });
 } else {
-  sequelize = new Sequelize(db, {
-    dialect: 'postgres',
-    ssl: true,
-    protocol: 'postgres',
-    dialectOptions: {
-      native: true,
-      ssl: { require: true, rejectUnauthorized: false },
-    },
-    logging: false,
-  });
+  sequelize = new Sequelize(db, {
+    dialect: 'postgres',
+    ssl: true,
+    protocol: 'postgres',
+    dialectOptions: {
+      native: true,
+      ssl: { require: true, rejectUnauthorized: false },
+    },
+    logging: false,
+  });
 }
 
 const WA_CONF = sequelize.define(
-  "WA_CONF",
-  {
-    id: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-    },
-    presence: {
-      type: DataTypes.STRING,
-      defaultValue: "rien",
-    },
-    lecture_status: {
-      type: DataTypes.STRING,
-      defaultValue: "non",
-    },
-    like_status: {
-      type: DataTypes.STRING,
-      defaultValue: "non",
-    },
-    dl_status : {
-      type: DataTypes.STRING,
-      defaultValue: "non",
-    },
-    antivv: {
-      type: DataTypes.STRING,
-      defaultValue: "non",
-    },
-    antidelete: {
-      type: DataTypes.STRING,
-      defaultValue: "non",
-    },
-    mention: {
-      type: DataTypes.STRING,
-      defaultValue: "1",
-    },
-  },
-  {
-    tableName: "wa_conf",
-    timestamps: false,
-  }
+  "WA_CONF",
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    presence: {
+      type: DataTypes.STRING,
+      defaultValue: "rien",
+    },
+    lecture_status: {
+      type: DataTypes.STRING,
+      defaultValue: "non",
+    },
+    like_status: {
+      type: DataTypes.STRING,
+      defaultValue: "non",
+    },
+    dl_status : {
+      type: DataTypes.STRING,
+      defaultValue: "non",
+    },
+    antivv: {
+      type: DataTypes.STRING,
+      defaultValue: "non",
+    },
+    antidelete: {
+      type: DataTypes.STRING,
+      defaultValue: "non",
+    },
+    mention: {
+      type: DataTypes.STRING,
+      defaultValue: "1",
+    },
+  },
+  {
+    tableName: "wa_conf",
+    timestamps: false,
+  }
 );
 
 (async () => {
-  await WA_CONF.sync();
-  console.log("WA_CONF synchronisée.");
+  await WA_CONF.sync();
+  console.log("WA_CONF synchronisée.");
+ 
+  const rowsToUpdate = await WA_CONF.findAll({ where: { mention: "non" } });
+  for (const row of rowsToUpdate) {
+    row.mention = "1";
+    await row.save();
+  }
+  
 })();
 
 module.exports = { WA_CONF };

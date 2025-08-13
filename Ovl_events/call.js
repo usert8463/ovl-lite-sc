4d7a133d03ca4b97f1267472b289154dff282345
Ainz-devs <ainzdevs@gmail.com>
2025-08-13 00:31:14 +0000

    Update call.js

--- Ovl_events/call.js
+++ Ovl_events/call.js
@@ -8,7 +8,7 @@ async function call(ovl, callEvent) {
 
     if (!caller) return;
 
-    const config = await WA_CONF2.findOne({ where: { id: ovl.user.id } });
+    const config = await WA_CONF2.findOne({ where: { id: 1 } });
     if (!config || config.anticall !== "oui") return;
 
     await ovl.rejectCall(callId);

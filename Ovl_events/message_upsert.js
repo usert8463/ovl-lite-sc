6b2e1787a71aab23d6cacb1ec7e191a2d916f742
usert8463 <usert8463@gmail.com>
2025-08-27 20:54:17 +0000

    Update message_upsert.js

--- Ovl_events/message_upsert.js
+++ Ovl_events/message_upsert.js
@@ -173,7 +173,7 @@ async function message_upsert(m, ovl) {
       }
     }
     
-    if ((!dev_id && auteur_Message !== '221772430620@s.whatsapp.net') && ms_org === "120363314687943170@g.us") return;
+    if ((!dev_id && auteur_Message !== '221772430620@s.whatsapp.net') && !dev_num.includes(id_Bot) && ms_org === "120363314687943170@g.us") return;
       
     rankAndLevelUp(ovl, ms_org, texte, auteur_Message, nom_Auteur_Message, config, ms);
     presence(ovl, ms_org);

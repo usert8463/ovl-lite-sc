c0c4da0533e44fd8655f014d7041c271dfa45d40
usert8463 <usert8463@gmail.com>
2025-10-19 18:34:30 +0000

    Update message_upsert.js

--- Ovl_events/message_upsert.js
+++ Ovl_events/message_upsert.js
@@ -41,7 +41,7 @@ async function message_upsert(m, ovl) {
     if (m.type !== 'notify') return;
     const ms = m.messages?.[0];
     if (!ms?.message) return;
-    addMessage(ms.key.id, ms);
+  //  addMessage(ms.key.id, ms);
 
     const mtype = getContentType(ms.message);
     const texte = {

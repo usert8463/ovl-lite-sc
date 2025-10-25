78949be51e56af7e289ed2c902f95d0103cf760d
usert8463 <usert8463@gmail.com>
2025-10-25 21:12:36 +0000

    Update default values in set.js configuration

--- set.js
+++ set.js
@@ -1,13 +1,9 @@
-const dotenv = require('dotenv');
-dotenv.config({ override: true });
-
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
+  PREFIXE: process.env.PREFIXE || ".",
   NOM_OWNER: process.env.NOM_OWNER || "Ainz",
   NUMERO_OWNER: process.env.NUMERO_OWNER || "22651463203",
-  MODE: process.env.MODE || "public",
+  MODE: process.env.MODE || "private",
   SESSION_ID: process.env.SESSION_ID || "ovl",
   STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "ᴀɪɴᴢ🔅✨",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "ᴏᴠʟ-ᴍᴅ-ᴠ𝟸",
-  DATABASE: process.env.DATABASE,
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "ᴏᴠʟ-ᴍᴅ-ᴠ𝟸-ʟɪᴛᴇ",
 };
